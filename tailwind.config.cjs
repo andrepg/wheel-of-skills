ffee442f77c1ddecaa147abb8f05ff71da0a2fef
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-21 14:43:13 -0300

    Inclui primeira estrutura de projeto

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,tsx,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: {},
+};
