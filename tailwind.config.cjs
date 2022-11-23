5835bac8177ad150ff5bb426ae5c0b4b66a20f15
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-22 22:54:40 -0300

    📦 Include Tailwind as dependecy

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,tsx,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: {},
-};
