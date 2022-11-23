a23c0da8c1f24831e1a5597441d603369deb6f48
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-22 22:55:30 -0300

    🧩 Create Input, Label and Form components

---
+++ src/components/FormularioUsuario.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { reactive } from "vue";
+
+import InputLabel from "./InputLabel.vue";
+import InputForm from "./InputForm.vue";
+
+const parametros = reactive([
+  {
+    label: "Formas de renda",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+  {
+    label: "Sabe poupar",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+  {
+    label: "Sabe investir",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+  {
+    label: "Uso consciente do dinheiro",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+  {
+    label: "Círculo social",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+  {
+    label: "Objetivos e metas",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+  {
+    label: "Monitora as finanças",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+  {
+    label: "Pensamentos impulsionadores",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+  {
+    label: "Evita procrastinação",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+  {
+    label: "Agradece e celebra",
+    descricao:
+      "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
+    valor: 10,
+  },
+]);
+</script>
+
+<template>
+  <form
+    action=""
+    class="flex flex-col mx-auto w-full px-5 md:w-11/12 lg:w-10/12"
+  >
+    <template :key="parametro.label" v-for="parametro in parametros">
+      <InputLabel :for="'item_' + parametro.index" :value="parametro.label" />
+      <InputForm
+        :id="'item_' + parametro.index"
+        v-model="parametro.valor"
+        placeholder="5"
+        type="number"
+        class=""
+      />
+    </template>
+  </form>
+</template>
---
+++ src/components/InputForm.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+defineProps({
+  modelValue: [String, Number],
+});
+
+defineEmits(["update:modelValue"]);
+
+const input = ref(null);
+
+onMounted(() => {
+  if (input.value.hasAttribute("autofocus")) {
+    input.value.focus();
+  }
+});
+
+defineExpose({ focus: () => input.value.focus() });
+</script>
+
+<template>
+  <input
+    ref="input"
+    :value="modelValue"
+    class="form-input rounded border-0 bg-purple-200 focus:bg-purple-50 transition duration-250 ease-linear focus:outline-none focus:ring focus:ring-purple-900 text-purple-900"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
