f88971e04dc2add1ce4ca4738a41e12432e8f3e1
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-23 01:01:10 -0300

    💡 Include GraphIcon on FormGenerator

--- src/components/FormGenerator.vue
+++ src/components/FormGenerator.vue
@@ -4,27 +4,29 @@ import { reactive } from "vue";
 import InputLabel from "./InputLabel.vue";
 import InputForm from "./InputForm.vue";
 
+import GraphIcon from "./icons/GraphIcon.vue";
+
 const parametros = reactive([
   {
-    label: "Formas de renda",
+    label: "Diversificación de renta",
     descricao:
       "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
     valor: 10,
   },
   {
-    label: "Sabe poupar",
+    label: "¿Sabes ahorrar?",
     descricao:
       "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
     valor: 10,
   },
   {
-    label: "Sabe investir",
+    label: "¿Sabes invertir?",
     descricao:
       "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
     valor: 10,
   },
   {
-    label: "Uso consciente do dinheiro",
+    label: "¿Sabes usar su dinero de forma consciente?",
     descricao:
       "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
     valor: 10,
@@ -42,25 +44,25 @@ const parametros = reactive([
     valor: 10,
   },
   {
-    label: "Monitora as finanças",
+    label: "Monitora sus finanzas",
     descricao:
       "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
     valor: 10,
   },
   {
-    label: "Pensamentos impulsionadores",
+    label: "Pensamientos de impulsión",
     descricao:
       "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
     valor: 10,
   },
   {
-    label: "Evita procrastinação",
+    label: "Evitas procrastinación",
     descricao:
       "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
     valor: 10,
   },
   {
-    label: "Agradece e celebra",
+    label: "Agradece y celebra",
     descricao:
       "Uma breve descrição sobre este ponto para esclarecer ao usuário.",
     valor: 10,
@@ -69,19 +71,33 @@ const parametros = reactive([
 </script>
 
 <template>
-  <form
-    action=""
-    class="flex flex-col mx-auto w-full px-5 md:w-11/12 lg:w-10/12"
-  >
-    <template :key="parametro.label" v-for="parametro in parametros">
-      <InputLabel :for="'item_' + parametro.index" :value="parametro.label" />
-      <InputForm
-        :id="'item_' + parametro.index"
-        v-model="parametro.valor"
-        placeholder="5"
-        type="number"
-        class=""
-      />
-    </template>
+  <form>
+    <div class="w-full mb-3 md:columns-2 md:gap-8">
+      <div
+        class="flex flex-col gap-1 mb-3"
+        :key="parametro.label"
+        v-for="parametro in parametros"
+      >
+        <InputLabel :for="'item_' + parametro.index" :value="parametro.label" />
+        <InputForm
+          :id="'item_' + parametro.index"
+          v-model="parametro.valor"
+          placeholder="5"
+          type="number"
+          class="w-full"
+        />
+      </div>
+    </div>
+
+    <div class="w-full">
+      <button
+        class="inline-flex items-center justify-center w-full gap-4 px-10 py-5 font-bold uppercase rounded shadow-md group bg-gradient-to-br text-purple-50 from-purple-600 to-purple-800 via-violet-600 grow"
+      >
+        <GraphIcon
+          class="w-10 stroke-purple-300 group-hover:animate-pulse"
+        />
+        <span class="text-xl text-left">QUIERO MI RESULTADO</span>
+      </button>
+    </div>
   </form>
 </template>
