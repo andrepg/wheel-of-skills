c02cde0ba1168c62233d2eb6e7261dccde1c59d4
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-23 01:01:35 -0300

    🔤 Increase label font size

--- src/components/InputLabel.vue
+++ src/components/InputLabel.vue
@@ -5,7 +5,7 @@ defineProps({
 </script>
 
 <template>
-  <label class="block font-medium text-sm text-purple-50">
+  <label class="block font-medium text-purple-50">
     <span v-if="value">{{ value }}</span>
     <span v-else><slot /></span>
   </label>
