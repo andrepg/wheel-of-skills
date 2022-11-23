521c71912dfc0c37d864a26e4c9e2ff7f6ce94b6
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-23 01:00:36 -0300

    🧩 Extract card glass in component

---
+++ src/components/CardGlass.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="relative w-full px-10 py-5 mx-auto rounded-lg shadow-md lg:w-8/12 border-purple-50 bg-purple-900/60"
+  >
+    <div
+      class="absolute top-0 left-0 z-0 w-full h-full bg-purple-800/20 blur-lg"
+    ></div>
+
+    <div class="relative z-10">
+      <slot />
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <script setup>
 import FormGenerator from "@/components/FormGenerator.vue";
+import CardGlass from "@/components/CardGlass.vue";
 </script>
 
 <template>
-  <FormGenerator />
+  <CardGlass>
+    <h1 class="mb-5 text-3xl text-white">
+      Para empezar...
+      <small class="block text-xl"
+        >necesitamos que nos conteste algunas preguntas.</small
+      >
+    </h1>
+
+    <FormGenerator />
+  </CardGlass>
 </template>
