60bf90b53811ca28e5db4f9776626874d3594f8e
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-22 22:55:06 -0300

    🧩 Create NavigationComponent

---
+++ src/components/NavigationComponent.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <nav
+    class="fixed top-0 z-10 flex px-10 py-5 bg-purple-300 text-purple-900 w-full shadow-lg"
+  >
+    <a href="#">Gráfico de Habilidades</a>
+  </nav>
+</template>
