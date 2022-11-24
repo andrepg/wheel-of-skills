f6b16ba7f544ec38b6182d54925bd6d1299ef4ce
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-24 01:00:22 -0300

    🔒 Add id to each question

--- src/data/questions.js
+++ src/data/questions.js
@@ -6,8 +6,13 @@ const newQuestion = (parametros) =>
     ...parametros,
   });
 
-const newQuestionOpinionScale = (questionTitle, questionDescription) =>
+const newQuestionOpinionScale = (
+  questionId = "",
+  questionTitle,
+  questionDescription
+) =>
   newQuestion({
+    id: questionId,
     title: questionTitle,
     subtitle: questionDescription,
     type: QuestionType.OpinionScale,
@@ -18,38 +23,47 @@ const newQuestionOpinionScale = (questionTitle, questionDescription) =>
 
 const FormQuestions = [
   newQuestionOpinionScale(
+    "diversificacionRenta",
     "Tu nivel de diversificación de renta",
     "Evalúa tu nivel de diversificación de renta actual, es decir, si tu renta proviene de varias fuentes o no."
   ),
   newQuestionOpinionScale(
+    "habilidadAhorro",
     "Tu habilidad de ahorrar mensualmente",
     "Evalúa tu habilidad de mantener un plan de ahorro mensual, guardando parte de tu renta para reinversión"
   ),
   newQuestionOpinionScale(
+    "habilidadInversiones",
     "Tu habilidad de inversiones y generar renta",
     "Evalúa tu habilidad de generar renta y buscar nuevas inversiones para diversificación"
   ),
   newQuestionOpinionScale(
+    "evaluacionCirculoSocial",
     "Evaluación de su círculo social y usted",
     "Evalúa cuánto tu círculo social está alineado con tus objetivos y metas financieras, además de tu alineamiento de ideales"
   ),
   newQuestionOpinionScale(
+    "habilidadCrearObjetivos",
     "¿Cuál tu habilidad de crear objetivos y cumplir sus metas?",
     "Evalúa si tienes disciplina para crear planes de ahorro, metas de vida y cumplir tus metas establecidas"
   ),
   newQuestionOpinionScale(
+    "rastreoFinanzas",
     "Tu rastreo de finanzas, gastos y costos",
     "Evalúa el nivel de rastreo de finanzas, gastos mensuales, inversiones, costos y todo el flujo financiero de tu mes"
   ),
   newQuestionOpinionScale(
+    "nivelResistenciaImpulsion",
     "¿Cuál tu nivel de resistencia a la impulsión?",
     "Evalúa tu nivel de resistencia a la impulsión de comprar algo que no sea necesario, o que sea superfluo"
   ),
   newQuestionOpinionScale(
+    "nivelEvitarProcrastinacion",
     "¿Cuánto evitas la procrastinación?",
     "Evalúa tu facilidad de evitar la procrastinación cuándo estés con cosas en tu lista y necesitas cumplir tus objetivos"
   ),
   newQuestionOpinionScale(
+    "nivelGratitudCelebracion",
     "Tu nivel de gratitud y celebración",
     "¿Cuál tu nivel de gratitud y celebración cuando alcanzas tu objetivo o meta?"
   ),
