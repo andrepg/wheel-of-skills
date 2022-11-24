6df874eaf860a6e535bac9f34ff571c1f283f486
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-24 01:00:59 -0300

    🧩 Include new ChartView component

--- src/router/index.js
+++ src/router/index.js
@@ -10,14 +10,13 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue"),
-    // },
+
+    {
+      path: "/chart/:hash",
+      name: "chart",
+      props: true,
+      component: () => import("../views/ChartView.vue"),
+    },
   ],
 });
 
---
+++ src/views/ChartView.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Olá, {{ props.hash }}</h1>
+</template>
+
+<script setup>
+import { onMounted, computed } from "vue";
+import CryptoJS from "crypto-js";
+
+const props = defineProps({
+  hash: String,
+});
+
+const decryptedData = computed(() =>
+  CryptoJS.AES.decrypt(props.hash, "SoyCaroMolina")
+).toString(CryptoJS.enc.Utf8);
+
+onMounted(() => console.log(decryptedData.toString()));
+</script>
