c63bdad937ce4eee6b0ae4361fb2595510289df3
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-25 15:27:40 -0300

    🔐 Include ChartView and decrypt function

--- src/views/ChartView.vue
+++ src/views/ChartView.vue
@@ -1,18 +1,39 @@
 <template>
-  <h1>Olá, {{ props.hash }}</h1>
+  <CardGlass>
+    <h1 class="mb-3.5 text-3xl text-left text-white">
+      <span class="font-bold">Acá está tu resultado</span>
+      <small class="block text-sm text-normal"
+        >Preparamos un gráfico especial para usted. Mientras lo analisas,
+        comparta la página con sus amigos en redes sociales, al fin de la página
+        o en la barra de menu superior</small
+      >
+    </h1>
+
+    <div class="relative w-full h-1 my-4 bg-purple-300 rounded-lg"></div>
+
+    <ChartGenerator :dataset="chartHistoryData" />
+  </CardGlass>
 </template>
 
 <script setup>
-import { onMounted, computed } from "vue";
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 import CryptoJS from "crypto-js";
 
-const props = defineProps({
-  hash: String,
+import CardGlass from "@/components/CardGlass.vue";
+import ChartGenerator from "@/components/ChartGenerator.vue";
+
+const routerProvider = useRouter();
+
+const fnDecryptChartData = (chartData, password) =>
+  CryptoJS.AES.decrypt(chartData, password).toString(CryptoJS.enc.Utf8);
+
+const chartHistoryData = JSON.parse(
+  fnDecryptChartData(window.history.state.chart, "SoyCaroMolina")
+);
+
+onMounted(() => {
+  if (!chartHistoryData || chartHistoryData == "")
+    routerProvider.replace("home");
 });
-
-const decryptedData = computed(() =>
-  CryptoJS.AES.decrypt(props.hash, "SoyCaroMolina")
-).toString(CryptoJS.enc.Utf8);
-
-onMounted(() => console.log(decryptedData.toString()));
 </script>
