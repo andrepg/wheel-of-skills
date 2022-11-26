05f2b0a075e38a3080247e3189a942c0187865bc
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-25 21:47:33 -0300

    🔤 Add custom font to our project

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      heading: ["Poppins", "sans-serif"],
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+  plugins: [require("@tailwindcss/forms")],
 };
