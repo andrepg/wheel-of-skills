7762ded5b6293add3ee9c9d216090c4341d5e395
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-25 21:44:26 -0300

    🖼 Add pattern on card glass

--- src/components/CardGlass.vue
+++ src/components/CardGlass.vue
@@ -1,9 +1,20 @@
 <template>
   <div
-    class="relative w-11/12 px-10 py-5 mx-auto my-5 rounded-lg shadow-md border-purple-50 bg-purple-900/60"
+    class="relative w-11/12 p-12 px-2 pt-5 mx-auto my-5 rounded-lg shadow-md md:px-10 border-purple-50 backdrop-blur-sm bg-purple-800/90 overflow-clip"
   >
+    <div
+      id="backgroundPattern"
+      class="absolute top-0 left-0 w-full h-full"
+    ></div>
     <div class="relative z-10">
       <slot />
     </div>
   </div>
 </template>
+
+<style>
+#backgroundPattern {
+  @apply opacity-20;
+  background-image: url("data:image/svg+xml,%3Csvg width='80' height='80' viewBox='0 0 80 80' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.4'%3E%3Cpath d='M50 50c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10s-10-4.477-10-10 4.477-10 10-10zM10 10c0-5.523 4.477-10 10-10s10 4.477 10 10-4.477 10-10 10c0 5.523-4.477 10-10 10S0 25.523 0 20s4.477-10 10-10zm10 8c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm40 40c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+}
+</style>
