72418e459d77c66f69bd8cbfc7b2178c16083849
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-27 10:51:22 -0300

    👄 Change final Share version

--- src/components/navigation/ShareMenu.vue
+++ src/components/navigation/ShareMenu.vue
@@ -1,29 +1,59 @@
 <script setup>
+import { computed } from "vue";
 import FacebookIcon from "@/components/icons/FacebookIcon.vue";
 import TelegramIcon from "@/components/icons/TelegramIcon.vue";
 import WhatsAppIcon from "@/components/icons/WhatsAppIcon.vue";
 
 import ShareMenuIconLink from "@/components/navigation/ShareMenuIconLink.vue";
+
+import SocialShare from "@/data/social_networks.js";
+
+const facebookShareUrl = computed(() =>
+  SocialShare.facebook.share_link.replaceAll("%url", window.location.href)
+);
+
+const telegramShareUrl = computed(() =>
+  SocialShare.telegram.share_link.replaceAll("%url", window.location.href)
+);
+
+const whatsappShareUrl = (isMobile) => {
+  if (isMobile) {
+    return SocialShare.whatsapp.share_link_mobile.replaceAll(
+      "%textMessage",
+      window.location.href
+    );
+  }
+
+  return SocialShare.whatsapp.share_link_desktop.replaceAll(
+    "%textMessage",
+    window.location.href
+  );
+};
 </script>
 
 <template>
   <ul>
     <li>
-      <ShareMenuIconLink link="#">
+      <ShareMenuIconLink :href="facebookShareUrl">
         <template #icon><FacebookIcon /></template>
         <template #text>Compartir en Facebook</template>
       </ShareMenuIconLink>
     </li>
 
     <li>
-      <ShareMenuIconLink link="#">
+      <ShareMenuIconLink :link="whatsappShareUrl(true)" class="hidden md:flex">
+        <template #icon><WhatsAppIcon /></template>
+        <template #text>Compartir en WhatsApp</template>
+      </ShareMenuIconLink>
+
+      <ShareMenuIconLink :link="whatsappShareUrl(true)" class="md:hidden">
         <template #icon><WhatsAppIcon /></template>
         <template #text>Compartir en WhatsApp</template>
       </ShareMenuIconLink>
     </li>
 
     <li>
-      <ShareMenuIconLink link="#">
+      <ShareMenuIconLink :link="telegramShareUrl">
         <template #icon><TelegramIcon /></template>
         <template #text>Compartir en Telegram</template>
       </ShareMenuIconLink>
--- src/components/navigation/ShareMenuIconLink.vue
+++ src/components/navigation/ShareMenuIconLink.vue
@@ -10,13 +10,18 @@ const propsVue = defineProps({
 <template>
   <a
     :href="propsVue.link"
+    target="_blank"
     class="flex flex-row items-center gap-2 px-4 py-4 list-none transition bg-transparent border border-purple-700 rounded-lg shadow-md font-heading md:py-3 group hover:bg-purple-700 hover:shadow-lg"
   >
-    <span class="text-3x fill-purple-900 group-hover:fill-purple-50">
+    <span
+      class="text-2xl transition delay-100 fill-purple-900 group-hover:fill-purple-50"
+    >
       <slot name="icon" />
     </span>
 
-    <span class="sm:inline-flex md:hidden">
+    <span
+      class="transition delay-100 sm:inline-flex md:hidden group-hover:text-purple-50"
+    >
       <slot name="text" />
     </span>
   </a>
