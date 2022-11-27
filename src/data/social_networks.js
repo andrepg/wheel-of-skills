c2e1c2f08c562c1f23b0075eedec0eeb31c7e363
Andre Paul Grandsire <andrepg@users.noreply.github.com>
2022-11-27 10:49:27 -0300

    ⛓ Include new social networks links

---
+++ src/data/social_networks.js
@@ -0,0 +1,14 @@
+const SocialShare = {
+  facebook: {
+    share_link: "https://facebook.com/share.php?u=%url",
+  },
+  whatsapp: {
+    share_link_desktop: "https://web.whatsapp.com://send?text=%textMessage",
+    share_link_mobile: "whatsapp://send?text=%textMessage",
+  },
+  telegram: {
+    share_link: "https://t.me/share/url?url=%url",
+  },
+};
+
+export default SocialShare;
